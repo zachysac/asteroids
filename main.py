dd91cc623fa507c6d72497e645e998180e6feae7
zachysac <zachsmithley@icloud.com>
2025-02-04 12:59:35 -0700

    added framecap of 60 fps, clean up syntax

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-pygame.init
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill(color=(0,0,0))
         pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        #limit framerate to 60    
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
