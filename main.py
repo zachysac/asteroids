75e0617eaf7755ae722e6b22c0b4686b568548c0
zachysac <zachsmithley@icloud.com>
2025-02-06 17:42:26 -0700

    added player to main.py and created player instance

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 # throughout this file
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
+    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     while True:
         screen.fill(color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
